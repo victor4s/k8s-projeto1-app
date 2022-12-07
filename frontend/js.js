1827077163587a2625abc92f3aee4b55b98f1d8e
victor4s <victor.augusto.silva@outlook.com>
2022-12-07 09:47:08 -0300

    Realizando Correcoes

--- frontend/js.js
+++ frontend/js.js
@@ -5,7 +5,7 @@ $("#button-blue").on("click", function() {
     var txt_comentario = $("#comment").val();
 
     $.ajax({
-        url: "",
+        url: "ip pod mysql",
         
         type: "post",
         data: {nome: txt_nome, comentario: txt_comentario, email: txt_email},
